4a5f955cac7389a8d378f0a18ec608186c26f1dc
mcmooneyham <matthewcmooneyham@gmail.com>
2026-05-15 13:37:36 -0700

    Add: evaluate-reverse-polish-notation - submission-2

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-2.swift
@@ -0,0 +1,32 @@
+class Solution {
+    func evalRPN(_ tokens: [String]) -> Int {
+        
+       var stack: [Int] = []
+
+        for token in tokens {
+            switch token {
+            case "+":
+                let b = stack.removeLast()
+                let a = stack.removeLast()
+                stack.append(a + b)
+            case "-":
+                let b = stack.removeLast()
+                let a = stack.removeLast()
+                stack.append(a - b)
+            case "*":
+                let b = stack.removeLast()
+                let a = stack.removeLast()
+                stack.append(a * b)
+            case "/":
+                let b = stack.removeLast()
+                let a = stack.removeLast()
+                stack.append(a / b)
+            default:
+                stack.append(Int(token)!)
+            }
+        }
+
+        return stack[0]
+
+    }
+}
