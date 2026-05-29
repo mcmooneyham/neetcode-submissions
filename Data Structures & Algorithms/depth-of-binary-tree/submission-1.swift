3a4e8e644811bb5b1e6267f54292847604ca353b
mcmooneyham <matthewcmooneyham@gmail.com>
2026-05-29 11:54:43 -0700

    Add: depth-of-binary-tree - submission-1

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-1.swift
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     var val: Int
+ *     var left: TreeNode?
+ *     var right: TreeNode?
+ *     init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func maxDepth(_ root: TreeNode?) -> Int {
+        
+        guard root != nil else { return 0 }
+
+        
+        let leftDepth = maxDepth(root?.left)
+        let rightDepth = maxDepth(root?.right)
+
+
+        return max(leftDepth, rightDepth) + 1
+
+
+    }
+}
